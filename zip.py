97d0dd1844c9ca16906c7bee30c93c0229702ab7
Raul Bazan Sanchez <47358801+RaulBSanchez@users.noreply.github.com>
2020-02-27 14:02:37 -0500

    Added Function

--- zip.py
+++ zip.py
@@ -1,5 +1,14 @@
 import os, sys, tarfile
 
-my_tar= tarfile.open('test.tgz')
-my_tar.extractall(r'C:\Users\rbazan536\Desktop\test')
-my_tar.close()
+def unzip():
+    my_tar= tarfile.open('0.zip')
+    my_tar.extractall(r'C:\Users\rbazan536\Desktop\test')
+    my_tar.close()
+
+
+path=r"C:\Users\rbazan536\Desktop\rename_test"
+files = os.listdir(path)
+i = 1
+for index, file in enumerate(files):
+    os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(index), '.zip'])))
+    unzip()
